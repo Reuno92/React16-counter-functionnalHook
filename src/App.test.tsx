52a1d40561ecadd085333478349b167346fe9c7b
Renaud Racinet <renaud.racinet@estiam.com>
2019-11-06 21:54:27 +0100

    feat(enzyme): add enzyme dependencies

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,4 +6,7 @@ it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App/>, div);
     ReactDOM.unmountComponentAtNode(div);
+    const actual = 1;
+    const expected = 1;
+    expect(actual).toBe(expected);
 });
